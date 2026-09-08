c1db950c564f97bd9c0ced587dd3715f04dea092
Vishnu Vardhan Reddy Pothireddy <vishnuvardhanreddi2006@gmail.com>
2026-09-08 12:05:25 +0530

    Intial Commit

---
+++ src/components/footer.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+
+def footer_home():
+
+    st.markdown(f"""
+                <div style="margin-top: 2rem; display: flex; justify-content: center; align-items: center;">
+                    <p style="font-weight: bold; color:white;"> Created by VISHNU </p>
+                </div>
+            
+               """, unsafe_allow_html=True)
