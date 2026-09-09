ba99a03c6af70853208165704710100dc1e0a08c
Vishnu Vardhan Reddy Pothireddy <vishnuvardhanreddi2006@gmail.com>
2026-09-09 11:20:23 +0530

    Face Pipeline

--- src/screens/student_screen.py
+++ src/screens/student_screen.py
@@ -1,4 +1,30 @@
 import streamlit as st
+import numpy as np
+from PIL import Image
+
+from src.ui.base_layout import style_background_dashboard, style_base_layout
+from src.components.header import header_dashboard
+from src.components.footer import footer_dashboard
 
 def student_screen():
-    st.header('Student Screen')
+
+
+    style_background_dashboard()
+    style_base_layout()
+
+    c1, c2 = st.columns(2, vertical_alignment='center', gap='xxlarge')
+    with c1:
+        header_dashboard()
+    with c2:
+        if st.button('Go back to Home', type='secondary', key='teacher_login_back_btn', width='stretch'):
+            st.session_state['login_type'] = None
+            st.rerun()
+    
+    st.header('Login using FaceID', text_alignment='center')
+    st.space()
+    st.space()
+    photo_source = st.camera_input("Position your face in the center")
+
+    if photo_source:
+        photo_array = np.array(Image.open(photo_source))
+    footer_dashboard()
